225579c8a4d54e156d7594ed88762e63de04f44e
Joy Lin <vvlnote@hotmail.com>
2019-03-03 18:04:47 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   #your code here
-  contacts["Freddy Mercury"][:favorite_icecream_flavors].shift
+  contacts["Freddy Mercury"][:favorite_icecream_flavors].delete_if{|item| item == "strawberry"}
   contacts
   #remember to return your newly altered contacts hash!
   contacts
